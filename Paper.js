c1c08da41e29c71b4858db4bb4d0ac34e2ac56c5
DarkVoyd <80186457+DarkVoyd@users.noreply.github.com>
2021-06-23 13:12:18 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,18 @@
+class Paper{
+    constructor(x, y) {
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2
+        }
+        this.body = Bodies.circle(x, y, 69,options);
+        this.image = loadImage("paper.png");
+        World.add(world, this.body);
+    }
+    display(){
+        translate(this.body.position.x, this.body.position.y);
+        imageMode(CENTER);
+        image(this.image,0,0);
+    }
+}
